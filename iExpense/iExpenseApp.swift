7ea15eb89834d304c0622bf4c93051b14d8091bc
joy jain <joyshah204@gmial.com>
2023-12-06 11:17:47 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by JOY JAIN on 06/12/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
